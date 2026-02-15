e8b1af4e94bcd15b2bd8ef476dc3dc71ff0a3542
Grief436 <rmonta23@stu.southtexascollege.edu>
2026-02-14 20:30:04 -0600

    added profile and basic linkcard

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,31 @@
 <script setup>
 import { ref } from 'vue'
-const title = ref('Hello There ✋')
+import LinkCard from '@/components/LinkCard.vue'
+
+const profile = ref({
+  name: 'Richard Montalvo',
+  slogan: 'Just a student',
+  avatar: 'https://api.dicebear.com/9.x/fun-emoji/svg?seed=Brian'
+  
+})
 </script>
 
 <template>
-  <main class="flex flex-1 items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
+  <main class="flex min-h-screen flex-col items-center px-4 py-8">
+    <!-- Profile Header -->
+     <div class="mb-8 flex flex-col items-center text-center">
+      <div 
+        class="mb-4 h-24 w-24 overflow-hidden rounded-full border-4 border-green-400 shadow-lg shadow-green-500/20"
+    >
+     
+      <img
+       :src="profile.avatar"
+       :alt="profile.name"
+       class="h-full w-full object-cover"
+      />
+      </div>
+      <h1 class="mb-2 font-display text-2xl font-bold text-white sm:text-3xl">{{ profile.name }}</h1>
+      <p class="max-w-xs text-gray-300 sm:text-lg">{{ profile.slogan }}</p>
+     </div>
   </main>
 </template>
